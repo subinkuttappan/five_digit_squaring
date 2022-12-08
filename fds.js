6ddf33be6221754b5015ffd01e4a300a394a3b10
Subin Kuttappan Stellal Mary <subinkuttappan@gmail.com>
2022-12-08 15:06:57 +0530

    Update fds.js

--- fds.js
+++ fds.js
@@ -14,6 +14,7 @@ function generateNumber()
 		}
 	}
 	document.getElementById("fds-number").innerHTML = fiveDigitNumber;
+	document.getElementById("fds-square").innerHTML = '';
 }
 
 function generateSquare() 
