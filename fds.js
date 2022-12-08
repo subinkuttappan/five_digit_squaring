edbdb40e8ebb1ff9be4862f3fd0808237636cf89
Subin Kuttappan Stellal Mary <subinkuttappan@gmail.com>
2022-12-08 15:04:33 +0530

    Add files via upload

---
+++ fds.js
@@ -0,0 +1,22 @@
+let fiveDigitNumber = 11111;
+
+function generateNumber() 
+{
+	let validNumberObtained = false;
+
+	while(!validNumberObtained)
+	{
+		fiveDigitNumber = Math.floor(Math.random()*90000) + 10000;
+		console.log(fiveDigitNumber);
+		if(Math.floor(fiveDigitNumber / 10000) <= 5 && Math.floor((fiveDigitNumber%10000) / 1000) <= 5)
+		{
+			validNumberObtained = true;
+		}
+	}
+	document.getElementById("fds-number").innerHTML = fiveDigitNumber;
+}
+
+function generateSquare() 
+{
+	document.getElementById("fds-square").innerHTML = fiveDigitNumber*fiveDigitNumber;
+}
