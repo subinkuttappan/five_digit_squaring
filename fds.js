d2cbbd3f10464f5163f74eb6be129bd90d9cd550
Subin Kuttappan Stellal Mary <subinkuttappan@gmail.com>
2022-12-12 16:53:54 +0530

    Update fds.js

--- fds.js
+++ fds.js
@@ -1,20 +1,51 @@
 let fiveDigitNumber = 11111;
 
-function generateNumber() 
+function generateNumber(digits) 
 {
-	let validNumberObtained = false;
-
-	while(!validNumberObtained)
-	{
-		fiveDigitNumber = Math.floor(Math.random()*90000) + 10000;
-		console.log(fiveDigitNumber);
-		if(Math.floor(fiveDigitNumber / 10000) <= 5 && Math.floor((fiveDigitNumber%10000) / 1000) <= 5)
-		{
-			validNumberObtained = true;
-		}
-	}
-	document.getElementById("fds-number").innerHTML = fiveDigitNumber;
+	document.getElementById("fds-number-two").innerHTML = '';
+	document.getElementById("fds-number-three").innerHTML = '';
+	document.getElementById("fds-number-four").innerHTML = '';
+	document.getElementById("fds-number-five").innerHTML = '';
 	document.getElementById("fds-square").innerHTML = '';
+
+	let validNumberObtained = false;
+	switch(digits)
+	{
+		case 2 :
+			fiveDigitNumber = Math.floor(Math.random() * 90 + 10);
+			console.log(fiveDigitNumber);
+			document.getElementById("fds-number-two").innerHTML = fiveDigitNumber;
+			break;
+		case 3 :
+			fiveDigitNumber = Math.floor(Math.random() * 900 + 100);
+			console.log(fiveDigitNumber);
+			document.getElementById("fds-number-three").innerHTML = fiveDigitNumber;
+			break;
+		case 4 :
+			while(!validNumberObtained)
+			{
+				fiveDigitNumber = Math.floor(Math.random() * 9000 + 1000);
+				console.log(fiveDigitNumber);
+				if(Math.floor(fiveDigitNumber / 1000) <= 5)
+				{
+					validNumberObtained = true;
+				}
+			}
+			document.getElementById("fds-number-four").innerHTML = fiveDigitNumber;
+			break;
+		case 5 :
+			while(!validNumberObtained)
+			{
+				fiveDigitNumber = Math.floor(Math.random()*90000) + 10000;
+				console.log(fiveDigitNumber);
+				if(Math.floor(fiveDigitNumber / 10000) <= 5 && Math.floor((fiveDigitNumber%10000) / 1000) <= 5)
+				{
+					validNumberObtained = true;
+				}
+			}
+			document.getElementById("fds-number-five").innerHTML = fiveDigitNumber;
+			break;
+	}
 }
 
 function generateSquare() 
